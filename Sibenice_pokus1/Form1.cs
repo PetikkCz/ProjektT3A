026302665cbc8e700c80a0db55ecf8d9a236b58b
MáchaPetr <MchaPetr@V314>
2026-02-24 09:26:13 +0100

    Přidání inicializace hry a hlavního menu
    Byly přidány nové importy a metody pro inicializaci hry a vytvoření hlavního menu. Metoda `Form1_Load` byla odstraněna a nahrazena metodou `InitializeGame`, která nastavuje vlastnosti formuláře a volá další metody pro vytvoření uživatelského rozhraní, jako je hlavní menu, panel nástrojů a stavový pruh. V rámci metody `CreateMainMenu` byly přidány položky menu pro hru a nástroje s odpovídajícími událostmi pro kliknutí.

--- Sibenice_pokus1/Form1.cs
+++ Sibenice_pokus1/Form1.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace Sibenice_pokus1
 {
     public partial class Form1 : Form
@@ -5,15 +7,35 @@ namespace Sibenice_pokus1
         public Form1()
         {
             InitializeComponent();
+            InitializeGame();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void InitializeGame();
         {
+            this.Text = "Šibenice";
+            this.Size = new Size(800, 600);
+            this.StartPosition = FormStartPosition.CenterScreen;
 
+            CreateMainMenu();
+            CreateToolbar();
+            CreateStatusStrip();
+            CreateGamePanel();
         }
 
-        private void InitializeComponent()
+        private void CreateMainMenu()
         {
+            MenuStrip menuStrip = new MenuStrip();
+
+            ToolStripMenuItem hraMenuItem = new ToolStripMenuItem("Hra");
+            hraMenuItem.DropDownItems.Add("Nová hra", null, NovaHra_Click);
+            hraMenuItem.DropDownItems.Add("Konec", null, Konec_Click);
+
+            ToolStripMenuItem nastrojeMenuItem = new ToolStripMenuItem("Nástroje");
+            nastrojeMenuItem.DropDownItems.Add("Nastavení", null, Nastaveni_Click);
+
+            menuStrip.Items.Add(hraMenuItem);
+            menuStrip.Items.Add(nastrojeMenuItem);
+
 
         }
     }
